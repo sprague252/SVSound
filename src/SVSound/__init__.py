f56467222ede6da4a98606e1e2e7fe40c661422b
Mark Sprague <spraguem@ecu.edu>
2020-07-02 21:14:09 -0400

    Edits to __init__.py

--- src/SVSound/__init__.py
+++ src/SVSound/__init__.py
@@ -1,3 +1,3 @@
-"""This module contains a collection of programs to read and write Broadcast Wave files. Several proprietary file formats that include additional metadata chunks are supported. See the docstring of the wavefile package for details on the supported formats.
+"""This package contains a collection of programs to read and write Broadcast Wave files. Several proprietary file formats that include additional metadata chunks are supported. See the docstring of the wavefile module for details on the supported formats.
 """
 __all__ = ["wavefile", "recorders"]
